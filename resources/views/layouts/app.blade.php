721188425df7ad64200a605bd652fc65c344f8a8
younus93 <itsme@theyounus.com>
2016-03-19 13:04:02 +0530

    fixed footer links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,6 +39,19 @@
 </head>
 <body class="{{ app()->getLocale() }}">
 
+<script type="text/javascript">
+    window._pt_lt = new Date().getTime();
+    window._pt_sp_2 = [];
+    _pt_sp_2.push('setAccount,309ae5ae');
+    var _protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+    (function() {
+        var atag = document.createElement('script'); atag.type = 'text/javascript'; atag.async = true;
+        atag.src = _protocol + 'cjs.ptengine.com/pta_en.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(atag, s);
+    })();
+</script>
+
 @include('layouts.navbar')
 
 
